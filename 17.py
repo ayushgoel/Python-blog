8ecb63db19f758d06e64dd0e67d79895a9d69215
Ayush Goel <ayushgoel111@gmail.com>
2012-03-16 02:08:47 +0530

    added answer 17

---
+++ 17.py
@@ -0,0 +1,10 @@
+# try-except
+
+try:
+	import lxml as xml
+except ImportError:
+	import xml
+
+#Processing using xml
+
+# Note that after the execution of the above code, we have at least one module imported by the name xml.

0c0d904c20bd8eb44567b576e0bf52146d5cccb3
Ayush Goel <ayushgoel111@gmail.com>
2012-03-17 00:13:20 +0530

    Changed 15tester

--- 15tester.py
+++ 15tester.py
@@ -1,8 +1,6 @@
-
-
 def tryexcept():
     count = {}
-    with open("SETUP_INSTRUCTIONS.txt", "r") as f:
+    with open("15tester_data.txt", "r") as f:
         for l in f:
             for c in l:
                 try:
@@ -13,7 +11,7 @@ def tryexcept():
 
 def dictget():
     count = {}
-    with open("SETUP_INSTRUCTIONS.txt", "r") as f:
+    with open("15tester_data.txt", "r") as f:
         for l in f:
             for c in l:
                 count[c] = count.get(c,0) + 1
@@ -22,7 +20,7 @@ def dictget():
 def default():
     from collections import defaultdict
     count = defaultdict(int)
-    with open("SETUP_INSTRUCTIONS.txt", "r") as f:
+    with open("15tester_data.txt", "r") as f:
         for l in f:
             for c in l:
                 count[c] += 1
@@ -33,6 +31,6 @@ if __name__ == "__main__":
     t1 = Timer(tryexcept)
     t2 = Timer(dictget)
     t3 = Timer(default)
-    print "try ... except ...", t1.timeit(10000)
-    print "dict.get", t2.timeit(10000)
-    print "defaultdict", t3.timeit(10000)
+    print "try ... except ... : ", t1.timeit(10000)
+    print "dict.get : ", t2.timeit(10000)
+    print "defaultdict : ", t3.timeit(10000)
